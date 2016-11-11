445b4c4febd84210e9cb9837e8f9571dbc136245
Jonathan D Borgia <jonathanborgia@gmail.com>
2016-11-11 08:01:13 -0700

    completed getbyaddress. still working on successful fetch.

--- Unlivable/scr/data/UnlivableDAOImpl.java
+++ Unlivable/scr/data/UnlivableDAOImpl.java
@@ -37,41 +37,43 @@ public class UnlivableDAOImpl implements UnlivableDAO {
 		try (InputStream is = wac.getServletContext().getResourceAsStream(FILE_NAME);
 				BufferedReader buf = new BufferedReader(new InputStreamReader(is));) {
 			String line = buf.readLine();
+			line = buf.readLine();
 			while ((line = buf.readLine()) != null) {
 				List<Bedroom> bedrooms = new ArrayList<>();
 				String[] tokens = line.split(",");
 				Integer streetNum;
+				System.out.println(tokens[0]);
 				try {
-					streetNum = Integer.parseInt(tokens[1].replaceAll("[^\\d.]", ""));
+					streetNum = Integer.parseInt(tokens[0].replaceAll("[^\\d.]", ""));
 				} catch (Exception e1) {
 					throw new IllegalArgumentException("An address must have a number.");
 				}
-				String nsew = tokens[2];
-				String streetName = tokens[3];
-				String unit = tokens[4];
-				String city = tokens[5];
-				String stateAbbr = tokens[6];
-				Integer zip = Integer.parseInt(tokens[7].substring(0, 5).replaceAll("[^\\d.]", ""));
+				String nsew = tokens[1];
+				String streetName = tokens[2];
+				String unit = tokens[3];
+				String city = tokens[4];
+				String stateAbbr = tokens[5];
+				Integer zip = Integer.parseInt(tokens[6].substring(0, 5).replaceAll("[^\\d.]", ""));
 				String keyNum = createKeyNum(streetNum, zip, unit);
 				Integer numOfBr;
 				try {
-					numOfBr = Integer.parseInt(tokens[8]);
+					numOfBr = Integer.parseInt(tokens[7]);
 				} catch (Exception e) {
 					numOfBr = 0;
 				}
-				Double numOfBa = Double.parseDouble(tokens[9].replaceAll("[^\\d.]", ""));
-				Integer numOfFloors = Integer.parseInt(tokens[10].replaceAll("[^\\d.]", ""));
-				Double buildingSqft = Double.parseDouble(tokens[11].replaceAll("[^\\d.]", ""));
-				Double landSqft = Double.parseDouble(tokens[12].replaceAll("[^\\d.]", ""));
-				Double ceilingHeight = Double.parseDouble(tokens[13].replaceAll("[^\\d.]", ""));
-				Double stairSqft = Double.parseDouble(tokens[14].replaceAll("[^\\d.]", ""));
-				Double hallSqft = Double.parseDouble(tokens[15].replaceAll("[^\\d.]", ""));
+				Double numOfBa = Double.parseDouble(tokens[8].replaceAll("[^\\d.]", ""));
+				Integer numOfFloors = Integer.parseInt(tokens[9].replaceAll("[^\\d.]", ""));
+				Double buildingSqft = Double.parseDouble(tokens[10].replaceAll("[^\\d.]", ""));
+				Double landSqft = Double.parseDouble(tokens[11].replaceAll("[^\\d.]", ""));
+				Double ceilingHeight = Double.parseDouble(tokens[12].replaceAll("[^\\d.]", ""));
+				Double stairSqft = Double.parseDouble(tokens[13].replaceAll("[^\\d.]", ""));
+				Double hallSqft = Double.parseDouble(tokens[14].replaceAll("[^\\d.]", ""));
 				for (int i = 0; i < numOfBr; i++) {
 					Bedroom bedroom = new Bedroom();
-					Boolean attachedBa = Boolean.parseBoolean(tokens[16 + i * 4]);
-					Boolean closet = Boolean.parseBoolean(tokens[17 + i * 4]);
-					Double bedroomSqft = Double.parseDouble(tokens[18 + i * 4].replaceAll("[^\\d.]", ""));
-					Double closetSqft = Double.parseDouble(tokens[19 + i * 4].replaceAll("[^\\d.]", ""));
+					Boolean attachedBa = Boolean.parseBoolean(tokens[15 + i * 4]);
+					Boolean closet = Boolean.parseBoolean(tokens[16 + i * 4]);
+					Double bedroomSqft = Double.parseDouble(tokens[17 + i * 4].replaceAll("[^\\d.]", ""));
+					Double closetSqft = Double.parseDouble(tokens[18 + i * 4].replaceAll("[^\\d.]", ""));
 					bedroom.setAttachedBa(attachedBa);
 					bedroom.setCloset(closet);
 					bedroom.setBedroomSqft(bedroomSqft);
@@ -169,7 +171,9 @@ public class UnlivableDAOImpl implements UnlivableDAO {
 		while (sb.length() < 13) {
 			sb.append("x");
 		}
-		sb.append(unit.toUpperCase().subSequence(0, 4));
+		if(unit.length()>4){
+			sb.append(unit.toUpperCase().subSequence(0, 4));
+		}
 		while (sb.length() < 17) {
 			sb.append("x");
 		}
